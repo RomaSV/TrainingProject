5bed3ed764e401510fae1143052c39420cc4d1ed
RomaSV <sve4nikov.roma@yandex.ru>
2018-03-11 01:20:46 +0300

    task2 is done
    Changes:
    + reorganising code
    + implementation of '-i', '-v' and '-r' options
    + tests for new options
    + JavaDoc for class

--- src/task2/Grep.java
+++ src/task2/Grep.java
@@ -1,29 +1,80 @@
 package task2;
 
 import java.io.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Command-line util grep.
+ * Searching text data for lines that contain the  word  given.
+ * Options:
+ *      -r(regex)      instead of lines that contain  the  word
+ *                     prints  lines  that  matches  the  regex
+ *                     (CAN NOT BE  USED  WITH  OTHER  OPTIONS)
+ *      -i             ignore case
+ *      -v             prints lines that DON'T contain the word
+ * Example:
+ *      -i -v word file_name       (normal search-mode)
+ * Or:
+ *      -r "expression" file_name  (for regex-mode)
+ * Note, that regex itself MUST be quoted.
+ */
 public class Grep {
     public static void main(String[] args) throws IOException {
         int argsLen = args.length;
+        boolean isRegexp = false;
+        boolean ignoreCase = false;
+        boolean inverse = false;
+        String result;
+
         if (argsLen < 2) {
             throw new IllegalArgumentException(
                     String.format("Expected at least 2 arguments, %d given.", argsLen));
         }
+        for (int i = 0; i < argsLen - 2; i++) {
+            switch (args[i]) {
+                case "-r":
+                case "-regex":
+                    isRegexp = true;
+                    break;
+                case "-i":
+                    ignoreCase = true;
+                    break;
+                case "-v":
+                    inverse = true;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown option(s).");
+            }
+        }
+
         File input = new File(args[argsLen - 1]);
         BufferedReader reader = new BufferedReader(new FileReader(input));
-        String result = grep(reader.lines(), args[argsLen - 2]);
+        result = grep(reader.lines(), args[argsLen - 2], isRegexp, ignoreCase, inverse);
         reader.close();
 
-        System.out.print(result);
+        System.out.println(result);
     }
 
-    /**
-     * Simple search for words.
-     * @return string with all lines, which contain the word.
-     */
-    static String grep(Stream<String> lines, String word) {
-        return lines.filter(line -> line.contains(word)).collect(Collectors.joining("\n"));
+    static String grep(Stream<String> lines, Predicate<String> predicate) {
+        return lines.filter(predicate).collect(Collectors.joining("\n"));
+    }
+
+    static String grep(Stream<String> lines, String expression, boolean r, boolean i, boolean v) {
+        if (r && (i || v)) {
+            throw new IllegalArgumentException("You can't use -r with any other options.");
+        }
+
+        Predicate<String> predicate;
+        if (r) {
+            predicate = line -> line.matches(expression);
+        } else if (i) {
+            predicate = line -> v ^ line.toLowerCase().contains(expression.toLowerCase());
+        } else {
+            predicate = line -> v ^ line.contains(expression);
+        }
+
+        return grep(lines, predicate);
     }
 }
--- test/task2/GrepTest.java
+++ test/task2/GrepTest.java
@@ -11,17 +11,72 @@ public class GrepTest {
 
         BufferedReader reader = new BufferedReader(new FileReader(input));
         assertEquals("Lorem ipsum dolor sit amet,",
-                Grep.grep(reader.lines(), "Lorem ipsum"));
+                Grep.grep(reader.lines(), "Lorem ipsum", false, false, false));
         reader.close();
 
         reader = new BufferedReader(new FileReader(input));
         assertEquals("sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n" +
                         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                Grep.grep(reader.lines(), "dolore"));
+                Grep.grep(reader.lines(), "dolore", false, false, false));
         reader.close();
 
         reader = new BufferedReader(new FileReader(input));
-        assertEquals("", Grep.grep(reader.lines(), "Лорем"));
+        assertEquals("", Grep.grep(reader.lines(), "Лорем", false, false, false));
+        reader.close();
+    }
+
+    @Test
+    public void regexTest() throws IOException {
+        File input = new File("input/input2");
+
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        assertEquals("224.0.0.22                                   статический\n" +
+                        "224.0.0.251                                  статический\n" +
+                        "239.255.255.250                              статический\n" +
+                        "192.168.1.1           c8-2b-35-9a-a6-1e      динамический\n" +
+                        "192.168.1.132         00-11-92-b3-a8-0d      динамический\n" +
+                        "192.168.1.255         ff-ff-ff-ff-ff-ff      статический\n" +
+                        "224.0.0.22            01-00-5e-00-00-16      статический\n" +
+                        "224.0.0.251           01-00-5e-00-00-fb      статический\n" +
+                        "224.0.0.252           01-00-5e-00-00-fc      статический\n" +
+                        "239.255.255.250       01-00-5e-7f-ff-fa      статический",
+                Grep.grep(reader.lines(),
+                        "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}.*", true, false, false));
+
+        try {
+            Grep.grep(reader.lines(),"*[Meaningless Regex]*", true, true, false);
+            fail("Exception expected");
+        } catch (IllegalArgumentException exception) {
+            assertEquals("You can't use -r with any other options.", exception.getMessage());
+        }
+
+        reader.close();
+    }
+
+    @Test
+    public void ignoreCase() throws IOException {
+        File input = new File("input/input2");
+
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        assertEquals("192.168.1.1           c8-2b-35-9a-a6-1e      динамический\n" +
+                        "192.168.1.132         00-11-92-b3-a8-0d      динамический",
+                Grep.grep(reader.lines(),
+                        "ДИНАМИЧЕСКИЙ", false, true, false));
+        reader.close();
+    }
+
+    @Test
+    public void inverse() throws IOException {
+        File input = new File("input/input2");
+
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        assertEquals("Интерфейс: 127.0.0.1 --- 0x1\n\n" +
+                        "адрес в Интернете      Физический адрес      Тип\n\n\n" +
+                        "Интерфейс: 192.168.1.133 --- 0x1c\n\n" +
+                        "адрес в Интернете     Физический адрес       Тип\n\n" +
+                        "192.168.1.1           c8-2b-35-9a-a6-1e      динамический\n" +
+                        "192.168.1.132         00-11-92-b3-a8-0d      динамический",
+                Grep.grep(reader.lines(), "статический", false, false, true));
         reader.close();
     }
 }
